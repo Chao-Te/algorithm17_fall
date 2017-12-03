478ed9e9b133da04511aca3596365ac2813195e7
Chao-Te <chao-te@Chao-Tes-MacBook-Pro.local>
2017-12-03 16:31:19 +0800

    new file:   339193_PA1/readme.txt 	new file:   339193_PA1/ref 	modified:   pa1/my_test.out 	modified:   pa1/pa1.cpp 	modified:   pa1/pa1.out

--- pa1/pa1.cpp
+++ pa1/pa1.cpp
@@ -2,8 +2,13 @@
 #include <string>
 #include <iomanip>
 #include <fstream>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
 
 using namespace std;
+
+
 
 int main(int argc, char** argv)
 {
@@ -18,23 +23,23 @@ int main(int argc, char** argv)
     int upper_base = 0;
     int now_base = 0;
 
-    float sample_space =0 ; // = 1/(n_sample_pts-1)
-    float t0 = 0;           //t0 =t
-    float t1 = 0;           // t1 = 1-t
-    float upper_x = 0;
-    float upper_y = 0;
-    float upper_right_x = 0;
-    float upper_right_y =0 ;
+    double sample_space =0 ; // = 1/(n_sample_pts-1)
+    double t0 = 0;           //t0 =t
+    double t1 = 0;           // t1 = 1-t
+    double upper_x = 0;
+    double upper_y = 0;
+    double upper_right_x = 0;
+    double upper_right_y =0 ;
 
-    float* dyn_table = NULL;
-    float* out_pts = NULL;
+    double* dyn_table = NULL;
+    double* out_pts = NULL;
 
     // read file
     ifstream infile;
-    infile.open(in_file_name); //string to float stof string to int atoi
+    infile.open(in_file_name.c_str()); //string to float stof string to int atoi
     getline(infile, str);// get first line
     n_control_pts = atoi(str.c_str());
-    dyn_table = new float[n_control_pts*n_control_pts*2];
+    dyn_table = new double[n_control_pts*n_control_pts*2];
     for(int i = 0; i < n_control_pts; i++){//set hte first row of table
         getline(infile, str);
         white_pos = str.find(' ');
@@ -45,8 +50,8 @@ int main(int argc, char** argv)
     n_sample_pts = atoi(str.c_str());
     infile.close();
 
-    sample_space = 1.0/float(n_sample_pts-1);
-    out_pts = new float[n_sample_pts*2];
+    sample_space = (double)(1.0)/ (double)(n_sample_pts-1);
+    out_pts = new double[n_sample_pts*2];
 
     // dynamic programming 
     for(int k = 0; k < n_sample_pts ; k++){
@@ -70,12 +75,17 @@ int main(int argc, char** argv)
     
     //write out results
     ofstream outfile;
-    outfile.open(out_file_name);
+    outfile.open(out_file_name.c_str());
+    //FILE* fp = fopen( out_file_name.c_str(), "w" ); 
+    //cout << "writing file" <<endl;
     for(int i = 0; i < n_sample_pts; i++){
-        outfile << setprecision(2) << fixed << out_pts[i*2] << '	'
+        outfile << setprecision(2) << fixed << out_pts[i*2] << '\t'
                 << setprecision(2) << fixed << out_pts[i*2+1]
                 <<endl;
+        //cout << out_pts[i*2] <<endl;
+        //fprintf(fp, "%2f\t%2f\n", out_pts[i*2], out_pts[i*2+1]);
     }
+    //fclose(fp);
     outfile.close();
     //release memory
     delete[] dyn_table;
